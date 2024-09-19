6f071e4e8033df94c1041f9cccdaea585c29ecd9
Enzo <enzogacademico@gmail.com>
2024-09-19 11:44:36 -0300

    Criando script externo

---
+++ script.js
@@ -0,0 +1,66 @@
+// alert = "Olá mundo externo"
+
+// Declarando as variavéis
+
+var nome = "fiap";
+console.log(nome)
+
+let nome1 = "cidade"
+console.log(nome1)
+
+const nome2 = "javascript";
+console.log(nome2)
+
+// Declaração indefinida
+let nome3;
+console.log(nome3)
+
+//Declaração vazia
+let disciplina = null
+console.log
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tipos de variavéis 
+
+// Inteiro
+let idade = 18;
+
+// Flutuantes ou com casas decimais
+
+let valor = 6.40;
+console.log(valor)
+
+// STRING (TEXTO)
+
+let texto = "FIAP;"
+
+// Verdadeiro ou falso - Booleanos
+
+let aluno = true
+
+// ARRAY - Conjunto de informações
+
+let tecnologias = ["java", "C#", "javascript"]
+
+console.log(tecnologias)
+
+// Conversões
+
+let numeroFloat = 123.456;
+console.log(parseInt(numeroFloat));
+
+
+
+let numeroString = 123.456;
+console.log(parseInt(numeroFloat));
